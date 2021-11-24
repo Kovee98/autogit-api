0094adf420675d0550f2a5637837533e96d4c734
Joshua Kovalchik <kovee98@gmail.com>
2021-11-24 17:02:20 -0600

    update defaults

--- src/config.js
+++ src/config.js
@@ -2,14 +2,13 @@ const { TIME } = require('./utils/enums');
 const path = require('path');
 
 const config = {
-    port: process.env.PORT || 4000,
+    port: process.env.PORT || 3080,
     redis: {
         host: process.env.REDIS_HOST || 'localhost',
         port: Number(process.env.REDIS_PORT) || 6379,
         // db: 2,
     },
-    // maxAge: Number(process.env.MAX_AGE) || TIME.DAY,
-    maxAge: Number(process.env.MAX_AGE) || 2 * TIME.MINUTE,
+    maxAge: Number(process.env.MAX_AGE) || 1 * TIME.DAY,
     secureCookies: process.env.SECURE_COOKIES !== 'false',
     cookieKey: process.env.COOKIE_KEY,
     dbHost: process.env.DB_HOST,
